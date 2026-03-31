25dd274f0bbd44e1feae6cfe4596a28f5af003f3
danhdaden777 <minhdanh2824@gmail.com>
2026-03-31 08:16:30 +0700

    Create project

---
+++ src/main/java/CCPTMT/DoAn/QuanLyCaLam/QuanLyCaLamApplication.java
@@ -0,0 +1,13 @@
+package CCPTMT.DoAn.QuanLyCaLam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyCaLamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyCaLamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CCPTMT/DoAn/QuanLyCaLam/QuanLyCaLamApplicationTests.java
@@ -0,0 +1,13 @@
+package CCPTMT.DoAn.QuanLyCaLam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyCaLamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
