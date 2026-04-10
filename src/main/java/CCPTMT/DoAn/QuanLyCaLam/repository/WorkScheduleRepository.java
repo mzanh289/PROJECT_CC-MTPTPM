827490e16eb8ef0ab4c75441a6f077d8f9c16ff8
REI <nguyenhuynhtu292@gmail.com>
2026-04-10 09:05:13 +0700

    Revert "Feature/auth users"

--- src/main/java/CCPTMT/DoAn/QuanLyCaLam/repository/WorkScheduleRepository.java
+++ src/main/java/CCPTMT/DoAn/QuanLyCaLam/repository/WorkScheduleRepository.java
@@ -12,8 +12,6 @@ public interface WorkScheduleRepository extends JpaRepository<WorkSchedule, Inte
 
     List<WorkSchedule> findByUserUserId(Integer userId);
 
-    List<WorkSchedule> findByUserUserIdOrderByWorkDateAsc(Integer userId);
-
     List<WorkSchedule> findByWorkDateBetween(LocalDate fromDate, LocalDate toDate);
 
     Optional<WorkSchedule> findByUserUserIdAndWorkDate(Integer userId, LocalDate workDate);
